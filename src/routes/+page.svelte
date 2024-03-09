5f729092d6b18247bb948be06b4006977de584da
angelo <angeloauxtero17@gmail.com>
2024-03-09 22:07:55 +0800

    2nc commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,73 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Modal from './Components/Modal.svelte';
+    
+    let showModal = false;
+    export let num1 = 0;
+    export let num2 = 0; 
+    let result = 0; 
+  
+    
+    const calculateResult = () => {
+      result = num1 + num2; 
+      toggleModal();
+    }
+  
+    const toggleModal = () => {
+      showModal = !showModal;
+    }
+  </script>
+  <Modal {showModal} {result} on:click={toggleModal}/>
+  <main>
+    <div class="form">
+        <h3>Enter A Number To perform addition</h3>
+        <div class="details">
+            <span class="label-text">First Number:</span>
+            <input type="number" class="input input-bordered input-success w-full max-w-xs" bind:value={num1}/>
+        </div>
+        <div class="details">
+            <span class="label-text">Second Number:</span>
+            <input type="number" class="input input-bordered input-success w-full max-w-xs" bind:value={num2}/>
+        </div>
+        <button class="btn btn-success" on:click={calculateResult}>CALCULATE</button>
+    </div>
+    
+  </main>
+  
+  <style>
+    .form{
+        padding: 25px;
+        border: 1px solid white;
+        display: flex;
+        flex-direction: column;
+        align-items: center; 
+    }
+    span{
+        padding: 10px;
+    }
+    h3{
+        font-weight: bold;
+        color: black;
+        border-radius: 20px;
+        text-transform: uppercase;
+        padding: 20px;
+        background: #d3fff4;
+        margin-bottom: 10px;
+    }
+    main {
+        padding: 180px;
+        display: flex;
+        flex-direction: column;
+        align-items: center; 
+    }
+
+    .details {
+        margin-bottom: 10px; /* Adjust margin between detail sections */
+        display: flex;
+        flex-direction: column;
+        align-items: left; 
+    }
+    button{
+        font-weight: bold;
+        margin: 10px;
+    }
+</style>
---
+++ src/routes/Components/Modal.svelte
@@ -0,0 +1,39 @@
+<script>
+    let num1 = 0;
+    let num2 = 0;
+    export let result = num1 + num2;
+    export let showModal = false;
+    export let isPromo = false;
+  
+    // Watch for changes in num1 and num2 to update the result
+    $: result = num1 + num2;
+  </script>
+  
+  {#if showModal}
+    <div class="show" class:promo={isPromo}  on:click>
+      <div class="backdrop"  >
+      <kbd class="kbd kbd-lg">The answer is: {result}</kbd>
+     </div>
+  </div>
+  
+  {/if}
+  <style>
+    
+    .kbd-lg{
+          background-color: #b5ffee;
+          color: black;
+          border: 2px solid black;
+          font-weight: bold;
+      }
+    .show{
+      background-color: rgba(128, 128, 128, 0.5);
+      position: fixed;
+      height: 100%;
+      width: 100%;
+    }
+    .backdrop{
+      display: flex;
+      justify-content: center;
+      padding-top: 300px;
+    }
+  </style>
